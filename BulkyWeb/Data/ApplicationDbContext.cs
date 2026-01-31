fb6edaf4c3a78ef653955b65b20eaa6b1a64ad7f
TshepisoSolumed1 <TshepisoN@solumed.co.za>
2026-01-31 21:29:56 +0200

    TN ConnectionStrings,ApplicationDbContext and  Category
    TN ConnectionStrings,ApplicationDbContext and  Category

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using BulkyWeb.Data;
+using Microsoft.EntityFrameworkCore;
+using BulkyWeb.Models;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+
+
+}
